1391615da6fa2fa138f1e6ff73f9370f5790c6b4
fmcruz23 <feliciamcruz@bren.ucsb.edu>
2022-02-18 12:10:49 -0800

    added tab panels, nav bar, scatterplot and DT table

---
+++ R/practice.R
@@ -0,0 +1,26 @@
+# practice code 
+library(palmerpenguins)
+library(tidyverse)
+library(DT)
+
+
+# filter for body masses 
+body_mass_df <- penguins %>% 
+  filter(body_mass_g %in% 3000:4000)
+
+# scatterplot
+ggplot(na.omit(body_mass_df), 
+       aes(x = flipper_length_mm, y = bill_length_mm,
+           color = species, shape = species)) +
+  geom_point() +
+  scale_color_manual(values = c("#FEA346", "#B251F1", "#4BA4A4")) +
+  labs(x = "Flipper Length (mm)", y = "Bill Length (mm)", 
+       color = "Penguin Species", 
+       shape = "Penguin Species")
+
+
+
+# DT table 
+
+DT::datatable(data = penguins)
+
---
+++ myapp/app.R
@@ -0,0 +1,87 @@
+# load packages ----
+library(shiny)
+library(palmerpenguins)
+library(tidyverse)
+
+# user interface ----
+ui <- fluidPage(
+  navbarPage(
+    "TITLE HERE",
+    tabPanel("Background",
+             "Penguin photos go here"),
+    
+    tabPanel("Penguin Plots", 
+             tabsetPanel(
+               tabPanel("Scatterplot",
+                        # body mass slider input ----
+                        sliderInput(inputId = "body_mass",
+                                    label = "Select a range of body masses (g):",
+                                    min = 2700, max = 6300, value = c(3000, 4000)),
+                        # body mass output ----
+                        plotOutput(outputId = "bodyMass_scatterPlot")), 
+               tabPanel("Histogram"))),
+    
+    tabPanel("Antarctic Weather Plots",
+             tabsetPanel(
+               tabPanel("Weather Plot"),
+               tabPanel("Weather Table"))),
+    
+    tabPanel("Data Exploration",
+             "insert DT table here",
+             # placeholder for dt
+             DT::dataTableOutput(outputId = "data_table"))
+  ))
+
+
+# break ----------------
+
+
+
+# server instructions ---- 
+server <- function(input, output){
+ 
+  # filter body masses ----
+  body_mass_df <- reactive({
+
+    penguins %>%
+      filter(body_mass_g %in% input$body_mass[1]:input$body_mass[2])
+
+  })
+
+# render scatterplot ----
+  output$bodyMass_scatterPlot <- renderPlot({
+
+     # code to generate scatterplot here
+
+    ggplot(na.omit(body_mass_df()),
+           aes(x = flipper_length_mm, y = bill_length_mm,
+               color = species, shape = species)) +
+      geom_point(size = 4) +
+      scale_color_manual(values = c("Adelie" = "#FEA346",
+                                    "Chinstrap" = "#B251F1",
+                                    "Gentoo" = "#4BA4A4")) +
+      scale_shape_manual(values = c("Adelie" = 19,
+                                    "Chinstrap" = 17,
+                                    "Gentoo" = 15)) +
+      labs(x = "Flipper Length (mm)", y = "Bill Length (mm)",
+           color = "Penguin Species",
+           shape = "Penguin Species")
+
+  })
+   
+# DT
+  output$data_table <- DT::renderDataTable({
+    DT::datatable(penguins,
+                  options = list(pageLength = 5),
+                  caption = tags$caption(
+                    style = 'caption-side: top; text-align: left;',
+                    'Table 1: ', tags$em('Penguin data')))
+
+  })
+ 
+}
+
+
+# combine ui and server into an app ----
+shinyApp(ui = ui, server = server)
+
